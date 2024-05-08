20f195acde7e86a3b840e1d74786e237a71faac3
Connor <connorkasarda@gmail.com>
2024-05-08 17:48:12 -0600

    normalization and euler angles.

--- DragonBreath/include/Math/Quaternion.h
+++ DragonBreath/include/Math/Quaternion.h
@@ -32,22 +32,35 @@ namespace dragonbreath
         @brief Constructor with no arguments passed
         */
         Quaternion() : w(0.0), x(0.0), y(0.0), z(0.0) {}
-        // TODO Add initialization of euler angles
     
         /**
         @brief Constructor with w, x, y, z arguments passed
         */
         Quaternion(float _w, float _x, float _y, float _z)
             : w(_w), x(_x), y(_y), z(_z) {}
-        // TODO Add initialization of euler angles
 
         /**
+        @brief the identity quaternion (denoted "1").
 
+        Multiplying any quaternion q by the identity quaternion simply returns
+        the same quaternion q:
+                        
+                                    1 * q = q * 1 = q
+
+        I.e. the identity quaternion serves as a neutral element for quaternion
+        multiplications.
         */
         static const Quaternion identity;
 
         /**
+        @brief the zero quaternion (denoted "0").
 
+        Adding the zero quaternion to any quaternion q results in the same
+        quaternion q:
+
+                                    0 + q = q + 0 = q
+
+        The zero quaternion serves as an additive identity.
         */
         static const Quaternion zero;
 
@@ -79,6 +92,31 @@ namespace dragonbreath
         static Quaternion fromRollPitchYaw(float roll, float pitch, float yaw);
 
         /**
+        @brief Converts the components of this quaternion such that this
+        quaternion's resulting magnitude equals 1.
+
+        To do compute this, we need to divide each component by the magnitude
+        of the quaternion, denoted ||q|| = sqrt(w^2 + x^2 + y^2 + z^2). Thus,
+        the versor v (normalized quaternion) is equal to this:
+
+                                    v = q / ||q||
+
+        This function manipulates the components of this quaternion internally.
+        */
+        void normalize();
+
+        /**
+        @brief Produces a new quaternion that represents the normalized version
+        of this quaternion without changing any of the internal components.
+
+        This is used with the toEulerAngles() function such that we get this
+        quaternion's normalized form without changing the internals.
+
+        @return the normalized quaternion
+        */
+        Quaternion toNormalized() const;
+
+        /**
         @brief Produces a euler angle representation of the quaternion. Euler
         angles will consist of the roll, pitch, and yaw in radians.
 
@@ -107,22 +145,22 @@ namespace dragonbreath
         std::string toString() const;
     private:
         /**
-
+        @brief w is the scalar value or real part simply multiplied by 1
         */
         float w {};
 
         /**
-
+        @brief x is the coefficient multiplied by the quaternion unit i
         */
         float x {};
 
         /**
-
+        @brief y is the coefficient multiplied by the quaternion unit j
         */
         float y {};
 
         /**
-
+        @brief z is the coefficient multiplied by the quaternion unit k
         */
         float z {};
     }; // class Quaternion
